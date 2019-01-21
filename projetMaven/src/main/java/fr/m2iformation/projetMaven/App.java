22c1a1b18eaa4a09772b3db6dcb1f75942c40cb5
ayoub <ayoub@hotmail.fr>
2019-01-21 15:45:46 +0100

    mon projet jdbc

---
+++ projetMaven/src/main/java/fr/m2iformation/projetMaven/App.java
@@ -0,0 +1,13 @@
+package fr.m2iformation.projetMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
